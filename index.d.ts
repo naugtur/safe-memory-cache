f414d26382301d3d80fa7ae0c49ff66c2253ffe3
Till Sanders <mail@till-sanders.de>
2025-02-19 15:04:46 +0100

    Add TypeScript declarations
    Signed-off-by: Till Sanders <mail@till-sanders.de>

---
+++ index.d.ts
@@ -0,0 +1,45 @@
+declare module "safe-memory-cache" {
+  export interface SafeMemoryCacheOptions {
+    /**
+     * Maximum number of items to store in cache. When cache length is close to the limit, oldest
+     * items are removed to make more room.
+     */
+    limit: number;
+
+    /**
+     * Time in miliseconds within which an element should no longer be in cache if it was not
+     * accessed. Actual time is approximate and will be less or equal `maxTTL`.
+     */
+    maxTTL?: number;
+
+    /**
+     * Overrides the number of buckets used internally.
+     * @default 2
+     */
+    buckets?: number;
+
+    /**
+     * Calls the function with a storage bucket that's been removed.
+     */
+    cleanupListener?: (removedBucket: Map<string, unknown>) => void;
+
+    /**
+     * Keep items longer than the maxTTL if they are used.
+     */
+    retainUsed?: boolean;
+  }
+
+  export interface SafeMemoryCache<Value> {
+    set(key: string, value: Value): Value;
+    get(key: string): Value | undefined;
+    clear(): void;
+  }
+
+  export type SafeMemoryCacheStructure = {
+    [key: string]: unknown;
+  };
+
+  export function safeMemoryCache<Value>(
+    options: SafeMemoryCacheOptions,
+  ): SafeMemoryCache<Value>;
+}
